4f9c06ecc983a87101ca96d46e08e5e1cddcab9a
Dasun Batheegama <dasunbatheegama256@gmail.com>
2026-01-26 23:08:22 +0530

    Update notesRoutes.js

--- backend/server.js
+++ backend/server.js
@@ -1,26 +1,29 @@
-// import express from "express"
-const express = require("express");
+import express from "express"
+// const express = require("express");
+import notesRouter from "./routes/notesRoutes.js";
 
 const app = express();
+
+app.use("/api/notes", notesRouter);
 
 // What is an Endpoint?
 // An endpoint is a combination of a URL + HTTP method that lets the client interact with a specific resource.
 
-app.get("/api/notes", (req, res) => {
-    res.status(200).send("you got 20 notes");
-});
+// app.get("/api/notes", (req, res) => {
+//     res.status(200).send("you got 20 notes");
+// });
 
-app.post("/api/notes", (req, res) => {
-    res.status(201).json({"message": "Note created successfully"});
-});
+// app.post("/api/notes", (req, res) => {
+//     res.status(201).json({"message": "Note created successfully"});
+// });
 
-app.put("/api/notes/:id", (req, res) => {
-    res.status(200).json({"message": "Note updated successfully!"});
-});
+// app.put("/api/notes/:id", (req, res) => {
+//     res.status(200).json({"message": "Note updated successfully!"});
+// });
 
-app.delete("/api/notes/:id", (req, res) => {
-    res.status(200).json({"message": "Note deleted successfully!"});
-});
+// app.delete("/api/notes/:id", (req, res) => {
+//     res.status(200).json({"message": "Note deleted successfully!"});
+// });
 
 app.listen(5001, () => {
     console.log("Server is running on port 5001");
